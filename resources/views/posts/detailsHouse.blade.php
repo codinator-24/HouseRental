0fffa9b98e69573ec744ccb12d8ca10bbb61af06
YasharKurD <yasharomar16@gmail.com>
2025-05-09 21:20:52 +0300

    Put Blure Backround to Booking modal

--- resources/views/posts/detailsHouse.blade.php
+++ resources/views/posts/detailsHouse.blade.php
@@ -212,14 +212,14 @@
         </section>
     </div>
 
-    {{-- Booking Message Modal - NEW --}}
-    <div id="bookingMessageModal" class="fixed inset-0 z-[60] flex items-center justify-center bg-black bg-opacity-50"
+    {{-- Booking Modal--}}
+    <div id="bookingMessageModal" class="fixed inset-0 z-[60] flex items-center justify-center bg-opacity-50 backdrop-blur-sm"
         style="display: none;" role="dialog" aria-modal="true" aria-labelledby="bookingMessageModalTitle">
         <div class="bg-white rounded-lg shadow-xl overflow-hidden max-w-md w-full mx-4">
             <div class="px-6 py-4 bg-gray-100 border-b border-gray-200 flex justify-between items-center">
-                <h1 id="bookingMessageModalTitle" class="text-xl font-semibold text-gray-700">Send Booking Inquiry
+                <h1 id="bookingMessageModalTitle" class="text-xl font-semibold text-gray-700">Send Booking
                 </h1>
-                <button id="closeBookingMessageModalBtn" aria-label="Close booking inquiry modal"
+                <button id="closeBookingMessageModalBtn" aria-label="Close booking modal"
                     class="text-gray-500 hover:text-gray-700 text-2xl">×</button>
             </div>
 
