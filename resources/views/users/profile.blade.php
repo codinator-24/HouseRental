31bd5608574faac21320c4062187f76211db218f
YasharKurD <yasharomar16@gmail.com>
2025-05-23 22:56:55 +0300

    Remove Update IdCard

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -132,7 +132,7 @@
 
                 {{-- ID Card --}}
                 <div class="mb-6">
-                    <label for="IdCard" class="block text-sm font-medium text-gray-700 mb-2">ID Card</label>
+                    <p class="block text-sm font-medium text-gray-700 mb-2">ID Card</p>
                     @if ($user->IdCard)
                         @php
                             $idCardPath = $user->IdCard;
@@ -156,11 +156,6 @@
                     @else
                         <p class="text-sm text-gray-500 mt-1">No ID Card uploaded.</p>
                     @endif
-                    <input type="file" name="IdCard" id="IdCard"
-                        class="mt-2 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100" />
-                    @error('IdCard')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
                 </div>
 
                 {{-- Address --}}
